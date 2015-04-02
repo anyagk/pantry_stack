79d5025e5e7fbf35fb8ab064fc5d16ac36cbbed6
Anya Glowa-Kollisch <anyagk@gmail.com>
2015-04-02 17:24:50 -0400

    styling

--- app/assets/javascripts/templates/templates.js
+++ app/assets/javascripts/templates/templates.js
@@ -5,20 +5,20 @@ templates['fridge-list'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":fun
 
   return "<li>"
     + this.escapeExpression(((helper = (helper = helpers.food_name || (depth0 != null ? depth0.food_name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"food_name","hash":{},"data":data}) : helper)))
-    + " <button class='used'>Add to shopping list</button><button class='trash'>Delete</button></li> ";
+    + " <button class='used form-button'>Add to shopping list</button><button class='trash form-button'>Delete</button></li> ";
 },"useData":true});
 templates['search-results'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var helper;
 
   return "<li>"
     + this.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"name","hash":{},"data":data}) : helper)))
-    + " <button class='add'>Add this item</button></li> ";
+    + " <button class='add form-button'>Add this item</button></li> ";
 },"useData":true});
 templates['shopping-list'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var helper;
 
   return "<li>"
     + this.escapeExpression(((helper = (helper = helpers.food_name || (depth0 != null ? depth0.food_name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"food_name","hash":{},"data":data}) : helper)))
-    + " <button class='bought'>Add to fridge</button><button class='trash'>Delete</button></li> ";
+    + " <button class='bought form-button'>Add to fridge</button><button class='trash form-button'>Delete</button></li> ";
 },"useData":true});
 })();
