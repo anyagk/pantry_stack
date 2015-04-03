e6aa6c2ba25322b109a93dc5f17fe88c76b912b8
Anya Glowa-Kollisch <anyagk@gmail.com>
2015-04-03 17:44:55 -0400

    responsive finished

--- app/assets/javascripts/templates/templates.js
+++ app/assets/javascripts/templates/templates.js
@@ -3,9 +3,9 @@
 templates['fridge-list'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var helper;
 
-  return "<li><p class='food-list'>"
+  return "<div class=\"list-o-g\"><p class='food-list'>"
     + this.escapeExpression(((helper = (helper = helpers.food_name || (depth0 != null ? depth0.food_name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"food_name","hash":{},"data":data}) : helper)))
-    + " </p><button class='used form-button list-button'>Add to shopping list</button><button class='trash form-button list-button'>Delete</button></li> ";
+    + " </p><button class='used form-button list-button'>Add to shopping list</button><button class='trash form-button list-button'>Delete</button></div> ";
 },"useData":true});
 templates['search-results'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var helper;
@@ -17,8 +17,8 @@ templates['search-results'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":
 templates['shopping-list'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var helper;
 
-  return "<li><p class='food-list'>"
+  return "<div class=\"list-o-g\"><p class='food-list'>"
     + this.escapeExpression(((helper = (helper = helpers.food_name || (depth0 != null ? depth0.food_name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"food_name","hash":{},"data":data}) : helper)))
-    + "</p> <button class='bought form-button list-button'>Add to fridge</button><button class='trash form-button list-button'>Delete</button></li> ";
+    + "</p> <button class='bought form-button list-button'>Add to fridge</button><button class='trash form-button list-button'>Delete</button></div> ";
 },"useData":true});
 })();
